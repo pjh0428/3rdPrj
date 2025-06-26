a253b2904bd277319ab58286609bccce3cfdbd07
hanadajukki <lukanic2012@gmail.com>
2025-06-26 11:04:31 +0900

    KSH
    좀비 피격, 사망, 공격애니메이션

---
+++ 3rdPrj/Assets/Script/HitBox.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public enum BodyPart { Body, Head }
+    public EnemyFSM zombie;
+
+    public float demageMultiplier;
+
+    int bulletLayer;
+
+    void Start()
+    {
+        bulletLayer = LayerMask.NameToLayer("Bullet");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == bulletLayer)
+        {
+
+            
+
+
+            if (other.CompareTag("Head"))
+                demageMultiplier = 5;
+            if (other.CompareTag("Body"))
+                demageMultiplier = 1;
+            
+            int baseDamage = other.GetComponent<Bullet>().damaged;
+            int finalDamage = Mathf.RoundToInt(baseDamage * demageMultiplier);
+            zombie.HitEnemy(finalDamage);
+
+            Debug.Log("collider");
+        }
+
+    }
+    
+    
+
+}
